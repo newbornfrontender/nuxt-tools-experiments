1b53e67be97401c66e36c3367524226ee17035fd
Infant Frontender <newbornfrontender@gmail.com>
2018-09-28 21:50:00 +0300

    Добавил базовые файлы

---
+++ nuxt.config.js
@@ -0,0 +1,35 @@
+export default {
+  head: {
+    meta: [{
+      charset: 'utf-8',
+    }, {
+      name: 'viewport',
+      content: 'width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes',
+    }, {
+      'http-equiv': 'x-ua-compatible',
+      content: 'ie=edge',
+    }],
+  },
+
+  router: {
+    linkActiveClass: 'link-active',
+    linkExactActiveClass: 'link-exact',
+  },
+
+  build: {
+    extend (config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(jsx?|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /node_modules/,
+        });
+      };
+    },
+  },
+
+  loading: {
+    color: 'black',
+  },
+};
