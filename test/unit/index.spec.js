493f8195e089f758d0dd12c3f124eff613e2bd20
Infant Frontender <newbornfrontender@gmail.com>
2018-10-01 12:57:07 +0300

    ДÑобавил отступ

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -4,6 +4,7 @@ import Component from '@/pages/index.vue';
 describe('Страницы', () => {
   test('index.vue корректно отображает заголовок', () => {
     const wrapper = shallowMount(Component);
+
     expect(wrapper.find('h1').text()).toBe('Главная страница');
   });
 });
